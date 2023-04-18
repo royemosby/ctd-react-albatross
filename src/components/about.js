3c3ae55f618e29313bebf1977256c2faba413b8f
Roy Mosby <ego.madking@gmail.com>
2023-04-18 07:21:54 -0400

    edit grammar

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,8 @@ export function About() {
       <p>
         This app captures the basic requirements for the Code The Dream todo
         list app for the React courses. Please see the "Final Project Grading
-        Rubric" at the bottom of your class' wiki on the lessons repo.
+        Rubric" at the bottom of your class' wiki on the lessons repo for more
+        info.
       </p>
     </div>
   );
